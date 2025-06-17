f76dcec0178817fbbe471565f737dc790f0a45ad
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 12:42:20 +0200

    [bot] Update files for AppID: 2139020

---
+++ 2139020.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2139020
+-- Name: What the Fog
+addappid(2139020) -- What the Fog
+addappid(2139021,0,"02f0f2b85e4ca82606c390c02c614e8c9d90d912d1d8a36decf3865c9fea443a") -- Main Game Content (Windows Content)
+setManifestid(2139021,"1344958474985686758")
