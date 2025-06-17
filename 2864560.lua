c252cbbc85ce8b4ab14fc41f97c4d97ece9e341f
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 12:27:16 +0200

    [bot] Update files for AppID: 2864560

---
+++ 2864560.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2864560
+-- Name: Rune Factory: Guardians of Azuma
+addappid(2864560) -- Rune Factory: Guardians of Azuma
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
