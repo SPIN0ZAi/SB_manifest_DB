f7d730d2b61c765bf23d9cfeb6c10c83896fdbda
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 11:45:01 +0200

    [bot] Update files for AppID: 1709880

---
+++ 1709880.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1709880
+-- Name: Loco Motive
+addappid(1709880) -- Loco Motive
+addappid(1709881,0,"b43e55a4be57a87ddebc6885791528fec27b4941e0d0c6bd2b33cc7a07b329b6") -- Main Game Content (Windows Content)
+setManifestid(1709881,"5692867277095153211")
