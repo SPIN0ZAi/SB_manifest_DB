dfad74d70931ba31adee28b0834ac027bc514e09
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 12:16:36 +0200

    Add files via upload

---
+++ 3155730.lua
@@ -0,0 +1,16 @@
+
+-- MAIN APPLICATION
+addappid(3155730) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme -
+
+-- MAIN APP DEPOTS
+addappid(3155731, 1, "f5f3f46814cdec1b4afab690622f6dffb042619e839eb1b08310252f9099e0fc") -- Main Game Content (Windows Content)
+setManifestid(3155731, "5453751672566898454", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3199230) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Digital Deluxe Upgrade Pack
+addappid(3204050) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Plumeria Hair Ornament Hairstyles (Set of 6)
+addappid(3204060) -- Venus Vacation PRISM - DEAD OR ALIVE Xtreme - Cheongsam Outfits (Set of 6)
