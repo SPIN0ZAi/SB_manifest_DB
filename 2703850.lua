a4ba632f4b6af3dc63ccb08a5667b46775e3b045
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 11:41:48 +0200

    [bot] Update files for AppID: 2703850

---
+++ 2703850.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2703850
+-- Name: Rooftops & Alleys: The Parkour Game
+addappid(2703850) -- Rooftops & Alleys: The Parkour Game
+addappid(2703851,0,"e64721646710451cc632805df1472e27540a29c83d26bb7a225f91eb38d8e828") -- Main Game Content (Windows Content)
+setManifestid(2703851,"5551489055012161443")
