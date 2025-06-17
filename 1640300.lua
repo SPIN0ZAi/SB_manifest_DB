a9eb71037024d610e15b10cc1ba4570e0c421957
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 12:46:21 +0200

    [bot] Update files for AppID: 1640300

---
+++ 1640300.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1640300
+-- Name: Project Warlock II
+addappid(1640300) -- Project Warlock II
+addappid(1640301,0,"0a4a648a90ec7381d779c624da1caf5b9fec4e69b48d0aae04af2a4011dafe29") -- Main Game Content (Windows Content)
+setManifestid(1640301,"979212241812554687")
