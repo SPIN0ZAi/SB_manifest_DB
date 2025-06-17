d033c5fe17bf89586ec08d9652a3ff44b4871256
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 02:42:46 +0200

    [bot] Update files for AppID: 3601190

---
+++ 3601190.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3601190
+-- Name: Skating Rink Story
+addappid(3601190) -- Skating Rink Story
+addappid(3601191,0,"811578cf0f699654332b0dad4fcea2790ee64c5da2a033069ed601e865f54cc2") -- Main Game Content (Windows Content)
+setManifestid(3601191,"12651640442793427")
