85fe9e73f7b94cdd7eb6f40476a377f3598b36fa
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 02:49:57 +0200

    [bot] Update files for AppID: 3169520

---
+++ 3169520.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3169520
+-- Name: Warhammer 40,000: Space Marine - Master Crafted Edition
+addappid(3169520) -- Warhammer 40,000: Space Marine - Master Crafted Edition
+addappid(3169521,0,"9c327e47da8ad83eb864ec73bad3a78e2f7ff2844fd4cc6c3ed89e72725e9217") -- Main Game Content (Windows Content)
+setManifestid(3169521,"7937958727984927930")
