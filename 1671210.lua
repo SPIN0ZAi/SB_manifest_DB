e66c4465245d2571cd439bf828190ee4c324157c
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 12:38:11 +0200

    [bot] Update files for AppID: 1671210

---
+++ 1671210.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1671210
+-- Name: DELTARUNE
+addappid(1671210) -- DELTARUNE
+addappid(1671212,0,"d3b68663ee177c37ee1d5cb49de06cbf9d0d81b2b72ffdde18665f6db6c6ed9f") -- Main Game Content (Windows Content)
+setManifestid(1671212,"7730842116999772152")
