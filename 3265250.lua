133b45f39b82635e2ee623efe5cd4eaf1c758999
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-17 02:58:25 +0200

    [bot] Update files for AppID: 3265250

---
+++ 3265250.lua
@@ -0,0 +1,10 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3265250
+-- Name: MindsEye
+addappid(3265250) -- MindsEye
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(3265251,0,"4314da2fbb6b1038e0f8199b23f2f2c4ab77a7b1fb15289036bdc4fb0178a0a1") -- Main Game Content (Windows Content)
+setManifestid(3265251,"4817197013420520512")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
