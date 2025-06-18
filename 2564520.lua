3c6f48936a99f41fd676fdd936431459b1383abd
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-18 22:52:55 +0200

    [bot] Update files for AppID: 2564520

---
+++ 2564520.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2564520
+-- Name: Lost in Random: The Eternal Die
+addappid(2564520) -- Lost in Random: The Eternal Die
+addappid(3478380,0,"08b4f767f1591c4c96ec034dd7c78d0d442ac1d6c39586fff1ece5f94a7e4e6c") -- Depot 1 (3478380_8473010599245403699.manifest)
+setManifestid(3478380,"8473010599245403699")
+addappid(2564521,0,"4b679f8c7ffc59c1398369a7bb72987e78717c22d81ec186eb11bb0cc0e5f6e8") -- Depot 2 (2564521_388399390072352571.manifest)
+setManifestid(2564521,"388399390072352571")
