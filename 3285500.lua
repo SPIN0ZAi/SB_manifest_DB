4d78150a445b7bc6c8e9d87d018a3e298ede5e0f
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-18 23:56:22 +0200

    [bot] Update files for AppID: 3285500

---
+++ 3285500.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3285500
+-- Name: Three Kingdoms Mushouden
+addappid(3285500) -- Three Kingdoms Mushouden
+addappid(3786750,0,"8ea8b93dc597304b744fb21727c260bb3f2940dde0fc68d4cce2c9c1a2cdbecf") -- Depot 1 (3786750_541906025618108742.manifest)
+setManifestid(3786750,"541906025618108742")
+addappid(3285501,0,"d540ff5a3e564eb42dd7bc7fcd2767ba878e869bde88b3b99f21372cf43fe116") -- Depot 2 (3285501_6566154415776992185.manifest)
+setManifestid(3285501,"6566154415776992185")
