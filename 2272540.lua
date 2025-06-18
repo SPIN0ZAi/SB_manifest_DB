18578873f531d3368efa8b3294209a757d6d8741
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-18 23:00:19 +0200

    [bot] Update files for AppID: 2272540

---
+++ 2272540.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2272540
+-- Name: FBC: Firebreak
+addappid(2272540) -- FBC: Firebreak
+addappid(2272541,0,"a07aa761da92da0deb6a785fac179e2fc66fd3cb9b177051ff9b397b2b966ceb") -- Depot 1 (2272541_5720483882927387287.manifest)
+setManifestid(2272541,"5720483882927387287")
