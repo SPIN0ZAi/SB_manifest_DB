15e43695f9f1cc493a4227f597a5defcbdd26a62
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:37:37 +0200

    [bot] Update files for AppID: 3763770

---
+++ 3763770.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3763770
+-- Name: Reality | الواقع
+addappid(3763770) -- Reality | الواقع
+addappid(3763771,0,"a54b6be4bf66453c2d1d0349bce4efe08683d0ed572acbbd459c4438fb0db8fc") -- Depot 1 (3763771_4963971905888347113.manifest)
+setManifestid(3763771,"4963971905888347113")
