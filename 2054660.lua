6f7c766e0ab45f77f0b6eebe6ed2764ed64916d7
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:49:23 +0200

    [bot] Update files for AppID: 2054660

---
+++ 2054660.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2054660
+-- Name: Disney Illusion Island Starring Mickey and Friends
+addappid(2054660) -- Disney Illusion Island Starring Mickey and Friends
+addappid(2054661,0,"ab4e2da3364ed5edd189355519a244cf4f7630469abeb2ede70a411d2eeb5382") -- Depot 1 (2054661_6939096338627956475.manifest)
+setManifestid(2054661,"6939096338627956475")
