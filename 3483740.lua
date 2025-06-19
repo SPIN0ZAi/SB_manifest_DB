8de75a23805f12d8050d78d44819d95787d64120
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 18:34:20 +0200

    [bot] Update files for AppID: 3483740

---
+++ 3483740.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3483740
+-- Name: Cast n Chill
+addappid(3483740) -- Cast n Chill
+addappid(3483741,0,"0085e2ab6e91d273c72748b41db876429a43a28e359be3bd5519ddfa4d8ac501") -- Depot 1 (3483741_5456948244804082422.manifest)
+setManifestid(3483741,"5456948244804082422")
