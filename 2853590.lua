89774498f480f9f572e0d9fb8c1521201d252c2d
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 21:53:44 +0200

    [bot] Update files for AppID: 2853590

---
+++ 2853590.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2853590
+-- Name: Void War
+addappid(2853590) -- Void War
+addappid(2853591,0,"d5701bf19577767fbae5a71a1115ee2dc76fc65a52dcb920f924c98432166c46") -- Depot 1 (2853591_7123289996475238397.manifest)
+setManifestid(2853591,"7123289996475238397")
