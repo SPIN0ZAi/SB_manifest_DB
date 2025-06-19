b665d61a813cdded3f64724c915247c210d46f12
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:43:10 +0200

    [bot] Update files for AppID: 2511310

---
+++ 2511310.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2511310
+-- Name: Pro Cycling Manager 25
+addappid(2511310) -- Pro Cycling Manager 25
+addappid(2511311,0,"56267938cdc192f9a25be2daf6f935095c5f0b95d42612ca156ff74076ee7f74") -- Depot 1 (2511311_325234736884167379.manifest)
+setManifestid(2511311,"325234736884167379")
