f20cabf68f18bed4870bc906197581bd777ff10c
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:43:25 +0200

    [bot] Update files for AppID: 1172710

---
+++ 1172710.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1172710
+-- Name: Dune: Awakening
+addappid(1172710) -- Dune: Awakening
+addappid(1172719,0,"7b18d5460246bac4620753a4ed05b5f4e42c3c6ad1fce01a9508eb6a9fac5e08") -- Depot 1 (1172719_5663042031530293926.manifest)
+setManifestid(1172719,"5663042031530293926")
+addappid(1172711,0,"fca2f8a025b411ec4d846fe6bf97d1d0589a93bb56a2527fb857a4bfe4aac2b3") -- Depot 2 (1172711_254971224613886247.manifest)
+setManifestid(1172711,"254971224613886247")
