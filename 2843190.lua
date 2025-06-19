52ba491a548f3aa8c17a33f26bdfb29947c374fb
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 15:40:46 +0200

    [bot] Update files for AppID: 2843190

---
+++ 2843190.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2843190
+-- Name: Camper Van: Make it Home
+addappid(2843190) -- Camper Van: Make it Home
+addappid(2843191,0,"fba4bbaff05d9c2d954a673410b3ed2468e54afa6403fe20bacfdddd06d0bb0d") -- Depot 1 (2843191_2015700268657035899.manifest)
+setManifestid(2843191,"2015700268657035899")
