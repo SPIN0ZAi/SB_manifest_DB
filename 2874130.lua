3b9686af045c02db4f3c65ccbc85b0c305f44a6c
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:30:23 +0200

    [bot] Update files for AppID: 2874130

---
+++ 2874130.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2874130
+-- Name: Berserk or Die
+addappid(2874130) -- Berserk or Die
+addappid(2874131,0,"5ec5d7cebcd47f39bda56d4344f939d79aa357ed7fb5cdeb6b5b16ceaf539c94") -- Depot 1 (2874131_6809014508236582451.manifest)
+setManifestid(2874131,"6809014508236582451")
