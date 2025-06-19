1d560fba1bfd08c36e81b88c6198321636d5da77
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 18:07:45 +0200

    [bot] Update files for AppID: 3527290

---
+++ 3527290.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3527290
+-- Name: PEAK
+addappid(3527290) -- PEAK
+addappid(3527291,0,"f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05") -- Depot 1 (3527291_8648963255441635145.manifest)
+setManifestid(3527291,"8648963255441635145")
