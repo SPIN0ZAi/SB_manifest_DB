06188445c059aabd0b39f585f26a79cfececbae5
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-19 06:42:35 +0200

    [bot] Update files for AppID: 3533100

---
+++ 3533100.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3533100
+-- Name: Wrap House Simulator🌯
+addappid(3533100) -- Wrap House Simulator🌯
+addappid(3533101,0,"165e0dadd51768e263a833318ade634f7f7a76a7fcf9d32356eb4a03e64f0f90") -- Depot 1 (3533101_2487113437165272569.manifest)
+setManifestid(3533101,"2487113437165272569")
