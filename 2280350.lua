b9bcb64dfaf9a9cc7f2acfabb37c9649e51ba9cc
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 13:45:03 +0200

    [bot] Update files for AppID: 2280350

---
+++ 2280350.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2280350
+-- Name: Turbo Dismount® 2
+addappid(2280350) -- Turbo Dismount® 2
+addappid(2280351,0,"05912239d3da3436c0206a9c064eff71dd637b6a9b9e540ba91cd84f6548e541") -- Depot 1 (2280351_895020260773225096.manifest)
+setManifestid(2280351,"895020260773225096")
