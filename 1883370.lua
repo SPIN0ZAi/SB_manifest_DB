a9198603f939fce8896bdc3c33264912cc5a772c
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:06:29 +0200

    [bot] Update files for AppID: 1883370

---
+++ 1883370.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1883370
+-- Name: PEPPERED: an existential platformer
+addappid(1883370) -- PEPPERED: an existential platformer
+addappid(1883371,0,"93e18aed49925c7edf2d81e74ab25313320b3af7c9956d516afd62f5245792cd") -- Depot 1 (1883371_8201352859680871044.manifest)
+setManifestid(1883371,"8201352859680871044")
