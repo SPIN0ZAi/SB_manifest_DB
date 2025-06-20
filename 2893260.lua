690fff6084a6e151842569304e4c4861698cb088
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:06:59 +0200

    [bot] Update files for AppID: 2893260

---
+++ 2893260.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 2893260
+-- Name: Maliki : Poison Of The Past
+addappid(2893260) -- Maliki : Poison Of The Past
+addappid(2893261,0,"305319ae6ba3f1cf25e4ac55f706bdd8e5f63cc579c33b36ac451d2103262836") -- Depot 1 (2893261_7537249539091915925.manifest)
+setManifestid(2893261,"7537249539091915925")
