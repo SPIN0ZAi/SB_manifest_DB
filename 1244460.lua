b4f2c09de321fad4a5323a484ec675e8616525e9
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:07:09 +0200

    [bot] Update files for AppID: 1244460

---
+++ 1244460.lua
@@ -0,0 +1,10 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1244460
+-- Name: Jurassic World Evolution 2
+addappid(1244460) -- Jurassic World Evolution 2
+addappid(1244461,0,"f32fa164f046ff769b0eb4c8ca8c0841866087c40db3edbec4d5f79b299e5fbe") -- Depot 1 (1244461_3466130684209996467.manifest)
+setManifestid(1244461,"3466130684209996467")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- Depot 2 (228988_6645201662696499616.manifest)
+setManifestid(228988,"6645201662696499616")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Depot 3 (228990_1829726630299308803.manifest)
+setManifestid(228990,"1829726630299308803")
