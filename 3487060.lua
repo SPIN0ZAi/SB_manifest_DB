60c393fb393252dad0962838de953c6eebd17cd9
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 13:41:00 +0200

    [bot] Add new game files for AppID: 3487060

---
+++ 3487060.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3487060
+-- Name: Youtubers Life 3 - Stream Together!
+addappid(3487060) -- Youtubers Life 3 - Stream Together!
+addappid(3487061,0,"a45085f2752e82e06b9b112531e9a91d431e75d9ed1a041b26b6e1e498743b39") -- Depot 1 (3487061_7348208920996431229.manifest)
+setManifestid(3487061,"7348208920996431229")
