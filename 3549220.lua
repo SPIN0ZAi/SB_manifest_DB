511780e66df92d4a69c7189359e6abb9f975df00
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:06:42 +0200

    [bot] Update files for AppID: 3549220

---
+++ 3549220.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3549220
+-- Name: Tennis Manager 25
+addappid(3549220) -- Tennis Manager 25
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Depot 1 (228989_550968249685141759.manifest)
+setManifestid(228989,"550968249685141759")
+addappid(3549221,0,"160dae45b34c12ba41e25a4ba0940b0d7203c755e61eaf21c4f6f16509430245") -- Depot 2 (3549221_382586284050440862.manifest)
+setManifestid(3549221,"382586284050440862")
