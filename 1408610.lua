f3a82083463603abf32848826a41e5a2790d2fcf
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 17:53:45 +0200

    [bot] Update files for AppID: 1408610

---
+++ 1408610.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 1408610
+-- Name: Call of the Wild: The Angler™
+addappid(1408610) -- Call of the Wild: The Angler™
+addappid(1408612,0,"0df254da7c6699200feea9dd9b69b97e7275867dbc594c76c55f4324cc0597e5") -- Depot 1 (1408612_3261304106948050548.manifest)
+setManifestid(1408612,"3261304106948050548")
+addappid(1408613,0,"8b019c5dac3541d498213ef63801f591cb9b1fdf3cd28716f3d5ebd9e03bd03a") -- Depot 2 (1408613_6424194900758736514.manifest)
+setManifestid(1408613,"6424194900758736514")
