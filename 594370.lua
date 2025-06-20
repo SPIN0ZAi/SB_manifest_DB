8bb09f4128d15385552db1a745eae796684156b7
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:40:27 +0200

    [bot] Update files for AppID: 594370

---
+++ 594370.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 594370
+-- Name: Crazy Fishing
+addappid(594370) -- Crazy Fishing
+addappid(594371,0,"aaddaab486effbcc7af177f14440b5dd24eda08496512247f8ceaa2b44db8ef0") -- Depot 1 (594371_3695747739614361523.manifest)
+setManifestid(594371,"3695747739614361523")
