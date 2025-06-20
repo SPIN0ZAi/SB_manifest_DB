b1bce2e08d639e8ab67bfbd54da81e311c53dc55
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:28:24 +0200

    [bot] Update files for AppID: 284160

---
+++ 284160.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 284160
+-- Name: BeamNG.drive
+addappid(284160) -- BeamNG.drive
+addappid(284161,0,"0e77f1cd7bb3169d9885cc26c7d2ef9fffc02c8c49671bec7b7308dd6cf86229") -- Depot 1 (284161_5999992157778164633.manifest)
+setManifestid(284161,"5999992157778164633")
+addappid(284161,0,"0e77f1cd7bb3169d9885cc26c7d2ef9fffc02c8c49671bec7b7308dd6cf86229") -- Depot 2 (284161_6362241651092443255.manifest)
+setManifestid(284161,"6362241651092443255")
