18356dffaf5586244b2647af7593c7eff72117e2
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 13:32:40 +0200

    [bot] Add new game files for AppID: 3437820

---
+++ 3437820.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3437820
+-- Name: Wiped Clean
+addappid(3437820) -- Wiped Clean
+addappid(3437821,0,"3b0ff8abdfbee78841571fc092cc59b865ca4f285fc26b38c59afe6d724deac9") -- Depot 1 (3437821_1081142265517731172.manifest)
+setManifestid(3437821,"1081142265517731172")
