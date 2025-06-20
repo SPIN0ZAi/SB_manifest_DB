da703a55a054448d3f4afebf7becc81ae2b65d5d
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:06:55 +0200

    [bot] Update files for AppID: 518790

---
+++ 518790.lua
@@ -0,0 +1,8 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 518790
+-- Name: theHunter: Call of the Wild™
+addappid(518790) -- theHunter: Call of the Wild™
+addappid(518791,0,"c56059a25b086342342b536c815b9903ee3faa35b8358fe7556346fe60a9081e") -- Depot 1 (518791_4750431550559912784.manifest)
+setManifestid(518791,"4750431550559912784")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- Depot 2 (228988_6645201662696499616.manifest)
+setManifestid(228988,"6645201662696499616")
