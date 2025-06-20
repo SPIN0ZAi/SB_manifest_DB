876d5e1808c078c3286b48c94e90e0d7ad87c448
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 13:41:41 +0200

    [bot] Update files for AppID: 756800

---
+++ 756800.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 756800
+-- Name: Contraband Police
+addappid(756800) -- Contraband Police
+addappid(756801,0,"1e813879ae7ee6dc69988e440b40c97412996770a6720c6ffeed76214ac6ce07") -- Depot 1 (756801_3699121330906436328.manifest)
+setManifestid(756801,"3699121330906436328")
