dfc6cbeefe8ad091d82b5493f6ce6bbc409532a1
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-06-20 02:56:50 +0200

    [bot] Add new game files for AppID: 3355800

---
+++ 3355800.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by SB manifest
+-- AppID 3355800
+-- Name: Bank Simulator
+addappid(3355800) -- Bank Simulator
+addappid(3355801,0,"fabe54fdaf7a5a04910f223f36584ebb24fee7c50fa79167e55c6c5fcb1ecfa2") -- Depot 1 (3355801_8772994643005934085.manifest)
+setManifestid(3355801,"8772994643005934085")
