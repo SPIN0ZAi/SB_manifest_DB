3d126119c1af4bf1c1d5f148990795061f40ee17
SB <109903730+SPIN0ZAi@users.noreply.github.com>
2025-07-04 01:29:52 +0200

    Add files via upload

---
+++ 637650.lua
@@ -0,0 +1,40 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 637650 Manifest
+-- Name: FINAL FANTASY XV WINDOWS EDITION
+-- Generated: 2025-06-05 15:39:25
+-- Total Depots: 5
+-- Total DLCs: 6
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(637650) -- FINAL FANTASY XV WINDOWS EDITION
+
+-- MAIN APP DEPOTS
+addappid(637651, 1, "067cfbc8b2cdd98dec9619de67a9c50b91477b19e8220fe19adf16631383d1e6") -- FINAL FANTASY XV Content
+setManifestid(637651, "4290656803638294445", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- FFXV WINDOWS EDITION 4K Resolution Pack (AppID: 771560)
+addappid(771560)
+addappid(771561, 1, "1a825705887e4f8fe89c25a9c508cfcf4b7e8040bf8eae935def11aa8f5cdc7f") -- FFXV WINDOWS EDITION 4K Resolution Pack - FINAL FANTASY XV Bonus texture pack depot
+setManifestid(771561, "5488915167077228736", 0)
+
+-- FINAL FANTASY XV EPISODE ARDYN (AppID: 977610)
+addappid(977610)
+addappid(977611, 1, "d7ea2d4a26e6b83babdec6daf4fb919b775deafb8768cb8a55125a754bf558cb") -- FINAL FANTASY XV EPISODE ARDYN - FFXV DLC A depot
+setManifestid(977611, "1865728960517912488", 0)
+
+-- FINAL FANTASY XV Original Soundtrack (AppID: 3372340)
+addappid(3372340)
+addappid(3372341, 1, "db1c488ab6037f4f1153200605cad9d9f22ea12d99387c441c2d7d8e18c8251c") -- FINAL FANTASY XV Original Soundtrack - Main Content
+setManifestid(3372341, "5579186600670066646", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(787600) -- FFXV Fashion Collection
+addtoken(787600, "1694990638127720653")
+addappid(787610) -- FFXV 8700K Pack
+addappid(922570) -- FFXV Alien Shield
